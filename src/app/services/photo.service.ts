28f2e39bfcda4e38b882bc0925ff4a35238d5849
Alan Rios <alan.rios.hd@gmail.com>
2024-10-21 16:16:08 -0500

    Refactorizar servicio de fotos y cargar imágenes guardadas

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -9,7 +9,28 @@ import { Preferences } from '@capacitor/preferences';
 
 export class PhotoService {
   public photos: UserPhoto[] = [];
+  private PHOTO_STORAGE: string = "photos";
+
   constructor() { }
+
+  public async loadSaved() {
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify([])
+    });
+
+    const { value } = await Preferences.get({ key: this.PHOTO_STORAGE });
+    this.photos = (value ? JSON.parse(value) : []) as UserPhoto[];
+
+    for (let photo of this.photos) {
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
+  }
+
   public async addNewToGallery() {
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
@@ -17,10 +38,44 @@ export class PhotoService {
       quality: 100
     });
 
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
     this.photos.unshift({
       filepath: "soon...",
       webviewPath: capturedPhoto.webPath!
-    })
+    });
+  }
+
+  private async savePicture(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(cameraPhoto: Photo) {
+    const response = await fetch(cameraPhoto.webPath!);  // Cambiado a cameraPhoto para referenciar correctamente
+    const blob = await response.blob();
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64(blob: Blob): Promise<string | ArrayBuffer | null> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
   }
 }
 
